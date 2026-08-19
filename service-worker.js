4786322d789adf937a511cfb8ecd16b5dca1090f
nbettermood-max <nbettermood-max@users.noreply.github.com>
2026-08-19 09:07:39 +0300

    Force installed app updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="dogether-shell-v1";
+const CACHE="dogether-shell-v2";
 const SHELL=["/","/manifest.webmanifest","/icon-192.png","/icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
